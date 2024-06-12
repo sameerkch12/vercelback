f4c1dc4fdd6ddac38a392285852e2232913517cc
sameerkch12 <sameer.sc62@gmail.com>
2024-06-12 22:10:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { ndown } = require('nayan-media-downloader');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+app.post('/download', async (req, res) => {
+    const { url } = req.body;
+    try {
+        const data = await ndown(url);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
